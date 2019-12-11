ea59f1ef6785fbd0cdfb3be21568117cce8649bd
Jorge <jorge@leadaki.com>
2019-12-11 09:40:54 -0300

    prints

---
+++ prints.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+    fmt.Println("Hola" + "Mundo")
+    fmt.Println("Hola", "Mundo", "!!!")
+    fmt.Println("5+5 =", 5+5)
+    fmt.Println("7.5/3.2=", 7.5/3.2)
+    fmt.Println("7/3=", 7/3)
+    fmt.Println("7/3=", 7.0/3.0)
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
